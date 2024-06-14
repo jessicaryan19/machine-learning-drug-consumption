fde83d483df87afb99053576456aa08a08008576
obertcoy <laisobert2@gmail.com>
2024-06-14 22:08:46 +0700

    fix: added dependency version and more input to fit the model

--- streamlit.py
+++ streamlit.py
@@ -1,28 +1,81 @@
-# Age,Gender,Education,Country,Ethnicity,Nscore,Escore,Oscore,Ascore,Cscore,Impulsive,SS
-# use streamlit to create a web app
-# prompt the input of the user
-
 import streamlit as st
+import numpy as np  # For numerical operations
 from tensorflow.keras.models import load_model
 
+# Define mappings
+age_mapping = {
+    '18 - 24': -0.95197,
+    '25 - 34': -0.07854,
+    '35 - 44': 0.49788,
+    '45 - 54': 1.09449,
+    '55 - 64': 1.82213,
+    '65+': 2.59171
+}
+
+gender_mapping = {
+    'Female': 0.48246,
+    'Male': -0.48246
+}
+
+education_mapping = {
+    'Left School Before 16 years': -2.43591,
+    'Left School at 16 years': -1.73790,
+    'Left School at 17 years': -1.43719,
+    'Left School at 18 years': -1.22751,
+    'Some College,No Certificate Or Degree': -0.61113,
+    'Professional Certificate/Diploma': -0.05921,
+    'University Degree': 0.45468,
+    'Masters Degree': 1.16365,
+    'Doctorate Degree': 1.98437
+}
+
+country_mapping = {
+    'Australia': -0.09765,
+    'Canada': 0.24923,
+    'New Zealand': -0.46841,
+    'Other': -0.28519,
+    'Republic of Ireland': 0.21128,
+    'UK': 0.96082,
+    'USA': -0.57009
+}
+
+ethnicity_mapping = {
+    'Asian': -0.50212,
+    'Black': -1.10702,
+    'Mixed-Black/Asian': 1.90725,
+    'Mixed-White/Asian': 0.12600,
+    'Mixed-White/Black': -0.22166,
+    'Other': 0.11440,
+    'White': -0.31685
+}
+
 # Title
+st.title('Drug Consumption Prediction')
 
-st.title('Drug Comnsumption Prediction')
-
+# Age options
+age_options = list(age_mapping.keys())
 # Age
-age = st.number_input('Age', min_value=18, max_value=100, value=18, step=1)
+age = st.selectbox('Age', age_options)
 
+# Gender options
+gender_options = list(gender_mapping.keys())
 # Gender
-gender = st.selectbox
+gender = st.selectbox('Gender', gender_options)
 
+# Education options
+education_options = list(education_mapping.keys())
 # Education
-education = st.selectbox
+education = st.selectbox('Education Level', education_options)
 
+# Country options
+country_options = list(country_mapping.keys())
 # Country
-country = st.selectbox
+country = st.selectbox('Country', country_options)
 
+# Ethnicity options
+ethnicity_options = list(ethnicity_mapping.keys())
 # Ethnicity
-ethnicity = st.selectbox
+ethnicity = st.selectbox('Ethnicity', ethnicity_options)
 
 # Nscore
 nscore = st.number_input('Nscore', min_value=0, max_value=100, value=0, step=1)
@@ -45,23 +98,32 @@ impulsive = st.number_input('Impulsive', min_value=0, max_value=100, value=0, st
 # SS
 ss = st.number_input('SS', min_value=0, max_value=100, value=0, step=1)
 
-# Button
-
 # Prediction button
 if st.button('Predict'):
     try:
         # Load the model
         model = load_model('model.h5')
 
+        # Encode categorical inputs using mappings
+        age_encoded = age_mapping[age]
+        gender_encoded = gender_mapping[gender]
+        education_encoded = education_mapping[education]
+        country_encoded = country_mapping[country]
+        ethnicity_encoded = ethnicity_mapping[ethnicity]
+
+        # Prepare input data for prediction (both categorical and numerical features)
+        input_data = np.array([[age_encoded, gender_encoded, education_encoded, country_encoded, ethnicity_encoded,
+                                nscore, escore, oscore, ascore, cscore, impulsive, ss]])
+
         # Make prediction
-        prediction = model.predict([[age, nscore, escore, oscore, ascore, cscore, impulsive, ss]])
+        prediction = model.predict(input_data)
         
         # Output prediction
         st.write(f'The predicted drug consumption is: {prediction}')
-        
+
         # (Optional) If your model outputs probabilities
         if hasattr(model, 'predict_proba'):
-            probability = model.predict_proba([[age, nscore, escore, oscore, ascore, cscore, impulsive, ss]])
+            probability = model.predict_proba(input_data)
             st.write(f'The probability of the predicted drug consumption is: {probability}')
 
         # Show model summary
