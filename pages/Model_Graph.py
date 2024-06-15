6de0f63d8a6aaf71569472363f13756588931f6d
Jessica Ryan <jessicaryan0419@gmail.com>
2024-06-15 15:12:17 +0700

    Update Model_Graph.py

--- pages/Model_Graph.py
+++ pages/Model_Graph.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 st.title('Model Graph')
-
+st.subheader('Evaluation Matrix')
 st.image('output.png')
 
+st.subheader('Drug Graph Based on Usage')
 st.image('drugs.png')
 
+st.subheader('Training Accuracy')
 st.image('training_acc.png')
 
+st.subheader('Testing Accuracy')
 st.image('testing_acc.png')
